4abec4fc538bc0163b3fea680fb4d91d15e37add
Stefan Jucker <stefan.jucker@gmail.com>
2024-03-11 13:50:12 +0100

    Fix naming

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -16,7 +16,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("ZKMF2024 - Festführer"),
+        title: const Text("ZKMF2024 - Festprogramm"),
         actions: [
           IconButton(
             icon: const Icon(Icons.settings),
