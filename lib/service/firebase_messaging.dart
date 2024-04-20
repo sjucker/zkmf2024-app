4c85f020ca4d0e2db96e7466a24cdd2800160089
Stefan Jucker <stefan.jucker@gmail.com>
2024-04-20 14:17:20 +0200

    Make sure APNs token is available, otherwise wait some time

--- lib/service/firebase_messaging.dart
+++ lib/service/firebase_messaging.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -44,6 +46,23 @@ void unfavorite(String identifier) async {
 }
 
 void subscribeTo(String topic) async {
+  // make sure APNs token is available, otherwise wait some time
+  if (Platform.isIOS) {
+    String? apnsToken = await FirebaseMessaging.instance.getAPNSToken();
+    if (apnsToken == null) {
+      // wait a little bit and then try again
+      await Future<void>.delayed(
+        const Duration(
+          seconds: 2,
+        ),
+      );
+      apnsToken = await FirebaseMessaging.instance.getAPNSToken();
+      if (apnsToken == null) {
+        FirebaseCrashlytics.instance.log("could not receive APNs in given time.");
+      }
+    }
+  }
+
   await FirebaseMessaging.instance.subscribeToTopic(topic);
   await _box.write("topic-$topic", true);
 }
